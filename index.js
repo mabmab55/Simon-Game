1cac541847ce2f472cf926d03fde7c4320e6cc63
Mabmab <bambam_lacerda001@hotmail.com>
2023-02-10 10:07:41 -0300

    Separando funções

--- index.js
+++ index.js
@@ -6,34 +6,40 @@ function selectRandomColor() {
   let number = Math.floor(Math.random() * 4);
   let randomChosenColour = colours[number];
   gamePattern.push(randomChosenColour);
+  //Flash animation
   $("#" + randomChosenColour).fadeOut(100).fadeIn(100).fadeOut(100).fadeIn(100);
+
+  playSoundEffect(randomChosenColour);
+  return number;
+}
+
+function playSoundEffect(color) {
   let audio;
-  switch (randomChosenColour) {
+  switch (color) {
     case "green":
-      audio = new Audio("sounds/" + randomChosenColour + ".mp3");
+      audio = new Audio("sounds/" + color + ".mp3");
       audio.volume = 0.2;
       audio.play();
       break;
 
     case "red":
-      audio = new Audio("sounds/" + randomChosenColour + ".mp3");
+      audio = new Audio("sounds/" + color + ".mp3");
       audio.volume = 0.2;
       audio.play();
       break;
 
     case "yellow":
-      audio = new Audio("sounds/" + randomChosenColour + ".mp3");
+      audio = new Audio("sounds/" + color + ".mp3");
       audio.volume = 0.2;
       audio.play();
       break;
 
     case "blue":
-      audio = new Audio("sounds/" + randomChosenColour + ".mp3");
+      audio = new Audio("sounds/" + color + ".mp3");
       audio.volume = 0.2;
       audio.play();
       break;
     default:
       break;
   }
-  return number;
 }
